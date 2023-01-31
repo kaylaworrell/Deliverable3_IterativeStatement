7a88439a4bd71c5c1b90141d30ef16140f06fd46
Kayla Worrell <Kayla Worrell@KAYLAS-LAPTOP>
2023-01-31 18:10:46 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Deliverable3_IterativeStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer between 1 and 100: ");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.Write("Specify if series type is even or odd:");
+            string series = Console.ReadLine();
+
+            try
+            {
+                if (series == "odd")
+                {
+                    Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are:");
+                    for (int i = 1; i < input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                if (series == "even")
+                {
+                    Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are:");
+                    for (int i = 0; i < input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Enter a numeric value.");
+            }
+            
+        }
+    }
+}
